fe9ec9bd5fa560aaa31cf373105ec42357ca18be
LukasEEH <lukas.epple25@web.de>
2023-04-28 15:13:05 +0200

    Implemented basic input form

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,92 @@
 <script setup lang="ts">
-
 import { ref } from 'vue'
 
-const text = ref('');
+const input = ref('')
+const submitted = ref(false)
 
-function submit(){
-  console.log("submitted: " + text.value);
+const anrede = ref('')
+const titel = ref('')
+const vorname = ref('')
+const nachname = ref('')
+const geschlecht = ref('')
+
+function submit() {
+  submitted.value = true
+  console.log('submitted: ' + input.value)
 }
 
+function hide() {
+  submitted.value = false
+  console.log('hide')
+}
+
+function store() {
+  submitted.value = false
+  console.log('store')
+}
 </script>
 
 <template>
+  <h1 id="heading">Kontaktsplitter</h1>
 
-  <h1>Kontaktsplitter</h1>
-  
-  <p>Enter Text</p>
-  <input v-model="text">
-  <button @click="submit()">Submit</button>
-  
+  <div class="inputArea">
+    <h2>Enter Input</h2>
+    <input v-model="input" placeholder="Frau Prof. Dr. rer. nar. Melanie Mustermann" />
+    <button @click="submit()">Eingabe</button>
+  </div>
+
+  <div class="submittedArea">
+    <h2 v-if="submitted">
+      Bearbeiten der Eingabe: <button @click="hide()" v-if="submitted">Schließen</button>
+    </h2>
+  </div>
+
+  <div class="changingArea" v-if="submitted">
+    <p>Anrede:</p>
+    <input v-model="anrede" />
+
+    <p>Titel:</p>
+    <input v-model="titel" />
+
+    <p>Vorname:</p>
+    <input v-model="vorname" />
+
+    <p>Nachname:</p>
+    <input v-model="nachname" />
+
+    <p>Geschlecht:</p>
+    <input v-model="geschlecht" />
+
+    <button @click="store()" v-if="submitted">Speichern</button>
+  </div>
 </template>
 
 <style scoped>
+.inputArea {
+  width: 380px;
+  margin-top: 40px;
+}
+
+.submittedArea {
+  width: 380px;
+  margin-top: 40px;
+}
+
+.changingArea {
+  margin-top: 20px;
+}
+
+#heading {
+  font-size: 40pt;
+  font-weight: bolder;
+}
+
+input {
+  width: 75%;
+}
+
+button {
+  width: 20%;
+  float: right;
+}
 </style>
