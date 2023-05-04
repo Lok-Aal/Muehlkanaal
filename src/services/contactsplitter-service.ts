999ec28b72f938c7de2ad78c03ca4b4ab47e5576
Suuyaaaaaaa <i20016@hb.dhbw-stuttgart.de>
2023-05-04 22:20:23 +0200

    comments

--- src/services/contactsplitter-service.ts
+++ src/services/contactsplitter-service.ts
@@ -3,18 +3,28 @@ import { type Contact, type contact } from '../types/contact'
 import { titles } from './data-service'
 
 export function splitStringIntoContact(input: string) {
+  // Variables for default values in empty contacts
+  let firstname = ''
+  let lastname = ''
+  let title = ''
   let isDiverse = false
   let salutation = ''
   let gender: 'männlich' | 'weiblich' | 'divers' | '' = 'divers'
   let letter_salutation = ''
 
+  // Split the name into parts, for further assignment
   const nameParts = input.split(' ')
+
+  // Return input as lastname, if only one input is given
   if (nameParts.length === 1) {
+    lastname = nameParts[0]
+
+    // Create output object from type contact, to return
     const output: contact = {
-      salutation: '',
-      title: '',
-      firstname: '',
-      lastname: nameParts[0],
+      salutation: salutation,
+      title: title,
+      firstname: firstname,
+      lastname: lastname,
       gender: gender,
       letter_salutation: letter_salutation
     }
@@ -27,8 +37,8 @@ export function splitStringIntoContact(input: string) {
   nameParts.forEach((part) => {
     namePartsClean.push(part.replace('.', '').toLowerCase())
   })
-  console.log(nameParts, namePartsClean)
 
+  // Check for the gender, which is the first entry in the nameParts Array. If it doesn't match the given German Herr/Frau salutations the gender will be set to "divers"
   if (nameParts[0] === 'Herr') {
     salutation = 'Herr'
     letter_salutation = 'Sehr geehrter Herr'
@@ -59,17 +69,26 @@ export function splitStringIntoContact(input: string) {
       break
     }
   }
-  const title = mergeTitleFromArrayLastIndex(nameParts, isDiverse, lastTitleIndex)
 
+  // Set the title
+  title = mergeTitleFromArrayLastIndex(nameParts, isDiverse, lastTitleIndex)
+
+
+  // Extend the two salutations by adding the title.
+  // The salutation remain empty if the gender is set to "divers"
   if (title && !isDiverse) {
     salutation = `${salutation} ${title}`
     letter_salutation = `${letter_salutation} ${title}`
   }
 
+  // Split the remaining nameParts into the first- and lastname.
+  // Set the variables
   const nameObject = splitNameFromArrayIndex(nameParts, lastTitleIndex)
-  const firstname = nameObject.name
-  const lastname = nameObject.lastname
+  firstname = nameObject.name
+  lastname = nameObject.lastname
 
+
+  // Create the output object of type "contact", given the generated parameters
   const output: contact = {
     salutation: salutation,
     title: title,
@@ -78,10 +97,12 @@ export function splitStringIntoContact(input: string) {
     gender: gender,
     letter_salutation: letter_salutation
   }
-
   return output
 }
 
+// Splits the nameParts after the given index (after nameParts that belong to the title) into a name and surname
+// The name always is the first entry of the sliced string
+// The surname is created by joining the remaining nameParts
 function splitNameFromArrayIndex(array: string[], index: number) {
   const slicedArray = array.slice(index)
   const name = slicedArray[0]
@@ -89,6 +110,9 @@ function splitNameFromArrayIndex(array: string[], index: number) {
   return { name: name, lastname: surname }
 }
 
+
+// Merges all entries in nameParts that belong to the title.
+// If the isDiverse-flag is set, the slicing will begin from the 2nd entry, since the first is occupied for the gender specific salutation
 function mergeTitleFromArrayLastIndex(array: string[], isDiverse: boolean, index: number) {
   const slicedArray = array.slice(isDiverse ? 0 : 1, index)
   const outputString = slicedArray.join(' ')
